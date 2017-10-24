8685a28cfc5b2ca1449e1398c95f91092b1c7c52
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-10-24 22:40:06 +0200

    fixed title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Kollokvie Best</title>
+    <title>Kollikvie Best</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
