b0a16964063b569e25f4f27a5c7c427036da7ff5
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-10-25 13:27:56 +0200

    fixed id/class setup

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ session_start();
   <body>
     <nav class="navbar navbar-expand-md  navbar-light  fixed-top" id="mainNavbar" ><!--oppretter navbar-->
       <div class="container" id="navbarContainer">
-        <a id="mainColYlw"class="navbar-brand" href="#contBoxJumbo">Home</a> <!--Home knapp som laster index.html-->
+        <a class="navbar-brand mainColYlw" href="#contBoxJumbo">Home</a> <!--Home knapp som laster index.html-->
         <a class="navbar-brand" href="#"></a><!--Legger til logo på navbar-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <!--Legger til menytoggel knapp når siden er "liten"/dratt sammen-->
           <span class="navbar-toggler-icon"></span>
@@ -24,16 +24,16 @@ session_start();
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto"> <!--oppretter navbar Elementer-->
             <li class="nav-item active">
-              <a id="mainColYlw" class="nav-link" href="#contBox1">About</a>
+              <a class="nav-link mainColYlw" href="#contBox1">About</a>
             </li>
             <li class="nav-item active">
-              <a id="mainColYlw" class="nav-link" href="#contBox2">Projects</a>
+              <a class="nav-link mainColYlw" href="#contBox2">Projects</a>
             </li>
             <li class="nav-item active">
-              <a id="mainColYlw" class="nav-link" href="#contBox3">Portfolias</a>
+              <a class="nav-link mainColYlw" href="#contBox3">Portfolias</a>
             </li>
             <li class="nav-item active">
-              <a id="mainColYlw" class="nav-link" href="#contBox4">Forum</a>
+              <a class="nav-link mainColYlw" href="#contBox4">Forum</a>
             </li>
             <?php if(isset($_SESSION['id'])){
               echo '<li class="nav-item active">
@@ -44,7 +44,7 @@ session_start();
           <div class="pull-right"><!--må ha en ny div for at bruker knappen skal legges helt til høyere på navbar-->
             <ul class="navbar-nav mr-auto">
               <li class="nav-item dropdown nav-item-right">
-                <a  id="mainColYlw" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle mainColYlw" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <?php
                   if(isset($_SESSION['uname'])){
                     echo $_SESSION['uname'];
@@ -73,7 +73,7 @@ session_start();
           </div><!--slutten av "bruker" dropdown-->
           <form class="form-inline my-2 my-lg-0 d-block d-md-none"><!--søkefelt og søkeknapp må legges i en form tagg-->
             <input class="form-control mr-sm-2" type="search"data-toggle="collapse" placeholder="Search" aria-label="search" />
-            <button id="btnSearch"class="btn btn-outline-success my-2 y-sm-0" type="submit">Search</button>
+            <button id="btnSearch"class="btn my-2 y-sm-0" type="submit">Search</button>
           </form>
         </div>
       </div>
@@ -85,7 +85,7 @@ session_start();
           <div class="d-flex justify-content-center">
           <form class="form-inline my-2 my-lg-0"><!--søkefelt og søkeknapp må legges i en form tagg-->
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="search" />
-            <button class="btn btn-outline-success my-2 y-sm-0 bg-dark" type="submit">Search</button>
+            <button class="btn  my-2 y-sm-0 bg-dark" type="submit">Search</button>
           </form>
         </div>
       </div>
