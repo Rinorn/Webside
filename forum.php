94c199ca5df96720832fa9d7903cebce6af9aa35
Yaddu <vhermansen2@gmail.com>
2017-10-26 21:17:23 +0200

    fixed things

--- forum.php
+++ forum.php
@@ -10,20 +10,24 @@ include 'scripts/load_posts.php';
     <?php include 'templates/header.php'; ?>
   </head>
   <body>
-    <?php include 'templates/nav.php'; ?>
-    <form action="scripts/add_post.php" method="post">
-      <textarea name="message" rows="8" cols="80"></textarea>
-      <button type="submit">Add post</button>
-    </form>
-
-    <div>
-        <?php
-        for ($i = 0; $i < sizeof($posts); $i++) {
-            echo "<p>"."Time: ".$posts[$i]['time']."</p>";
-            echo "<p>"."Post: ".$posts[$i]['text']."</p>";
-        }
-        ?>
+    <div class="container-fluid text-center" id="contBox2">
+      <div class="contText">
+        <?php include 'templates/nav.php'; ?>
+        <div>
+            <?php
+            for ($i = 0; $i < sizeof($posts); $i++) {
+                echo "<p>"."Time: ".$posts[$i]['time']."</p>";
+                echo "<p>"."Post: ".$posts[$i]['text']."</p>";
+            }
+            ?>
+        </div>
+        <form action="scripts/add_post.php" method="post">
+          <textarea name="message" rows="8" cols="80"></textarea>
+          <button type="submit">Add post</button>
+        </form>
+      </div>
     </div>
+
     <?php include 'templates/footer.php'; ?>
   </body>
 </html>
