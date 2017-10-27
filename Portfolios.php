b2d82ea0cd475f5719311ce415ae6db05dd6742b
Ole-Henning Undrum <oleundrum@hotmail.com>
2017-10-27 20:33:52 +0200

    Added styling to about and portofolios

--- Portfolios.php
+++ Portfolios.php
@@ -9,30 +9,46 @@
   <body>
   <?php include 'templates/nav.php'; ?>
   <div class="row">
-    <div class="container-fluid col-md-12">
-      <div class="container-fluid text-center" id="aboutCont1">
+    <div class="container-fluid col-md-12 centered">
+      <div class="container-fluid text-center mainContainer" id="portoCont1">
         <div class="contText">
           <h1>Portfolios</h1>
           <p>
-            Our portfolios blablabla
-          </p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id tempus sapien. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit ornare aliquet. Morbi nec accumsan metus. Mauris tempus nec metus sed scelerisque. Integer sed cursus sapien, quis dapibus massa.
+            <br />
+            Donec quis sapien in tortor pulvinar venenatis eu ut metus. Aliquam dignissim ornare felis in convallis. Integer posuere ligula eget tellus lobortis imperdiet. Quisque augue nisi, consectetur nec metus id, tristique feugiat ligula.   Integer scelerisque sodales gravida. Donec congue ultricies magna ut tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec tempor eros velit, at eleifend augue rutrum at. Sed  iaculis feugiat gravida. Etiam vel click the person you want to read about.
         </div>
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-6">
-      <div class="container portoBoxPers">
-
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 portoPersBox" id="person1Link">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6 personPhoto">
+          </div>
+        </div>
+      </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 portoPersBox">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 personPhoto">
+        </div>
       </div>
     </div>
-    <div class="col-md-6">
-      <div class="container portoBoxPers">
-
+    <div class="col-lg-3 col-md-6 col-sm-6 portoPersBox">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 personPhoto">
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6 portoPersBox">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 personPhoto">
+        </div>
       </div>
     </div>
   </div>
-
+</div>
 
   <?php include 'templates/footer.php'; ?>
   </body>
