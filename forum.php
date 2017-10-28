50ad7192ba38da02fa004b1874140f74ae884136
Jørgen Ringstad <jurg771@gmail.com>
2017-10-28 16:40:48 +0200

    Added some buttons without funtionality and some css to make it easier to see the grid

--- forum.php
+++ forum.php
@@ -8,6 +8,8 @@ include 'scripts/load_posts.php';
   <head>
     <title>Forum</title>
     <?php include 'templates/header.php'; ?>
+    <!--CSS-->
+    <link rel="stylesheet" href="forumStyle.css">
   </head>
   <body>
     <div class="container-fluid text-center" id="contBox2">
@@ -16,14 +18,23 @@ include 'scripts/load_posts.php';
         <div>
             <?php
             for ($i = 0; $i < sizeof($posts); $i++) {
-                echo "<p>"."Time: ".$posts[$i]['time']."</p>";
-                echo "<p>"."Post: ".$posts[$i]['text']."</p>";
+
+                echo "<div class='container text-left col-sm-4' id='timeStyle'><p>"."Time: ".$posts[$i]['time']." ID: ".$posts[$i]['id']."</p></div>";
+                echo "<div class='row'>";
+                echo "<div class='col-sm-4'></div>";
+                echo "<div class='text-left col-sm-1' id='userStyle'><p>"."User: "."</p></div>";
+                echo "<div class='text-left col-sm-3' id='messageStyle'><p>"."Post: ".$posts[$i]['text']."</p></div>";
+                echo "<div class='btn-group-vertical'>";
+                echo "<button id='reply' class='btn btn-primary' type='reply'>Reply</button>";
+                echo "<button id='delete'class='btn btn-default btn-danger' type='delete'>Delete post</button>";
+                echo "</div>";
+                echo "</div>";
             }
             ?>
         </div>
         <form action="scripts/add_post.php" method="post">
           <textarea id="text_post" name="message" rows="8" cols="80"></textarea>
-          <button type="submit">Add post</button>
+          <button id="postSubmit" type="submit">Add post</button>
         </form>
       </div>
     </div>
